96cd351a8844fd43c0cda017d274bfe475383119
Marc Zaku <mzaku@hotmail.de>
2021-03-17 16:44:52 +0100

    local changes

--- Assets/Scripts/Broker/DebugPlayer.cs
+++ Assets/Scripts/Broker/DebugPlayer.cs
@@ -1,6 +1,63 @@
+using System;
+using Heroes;
 using UnityEngine;
+using UnityEngine.Networking;
 
 namespace Broker {
+	
+	public interface IEnemySpawner {
+		Enemy SpawnEnemy();
+	}
+
+	public interface IAnalytics {
+		
+	}
+
+	public class ServiceConfiguration {
+		public enum Analytics {
+			None,
+			SingularSDK,
+			DeltaDNA
+		}
+
+		public Analytics analytics;
+
+		public IAnalytics CreateAnalytics() {
+			switch (this.analytics) {
+				case Analytics.DeltaDNA:
+					return new DeltaDNAAnalytics();
+				case Analytics.None:
+					return new EmptyAnalytics();
+				default:
+					throw new SystemException();
+			}
+		}
+	}
+
+	public class EmptyAnalytics : IAnalytics {
+	}
+
+	public class DeltaDNAAnalytics : IAnalytics {
+	}
+
+	// Main or Dependency Injection
+	public class Main {
+		public ServiceConfiguration services;
+
+		int Awake() {
+			var array = new int[]{1, 2, 3, 4}                                               ; 
+			return array[2];
+		}
+	}
+
+	public class GameLogic : MonoBehaviour {
+		public IEnemySpawner enemySpawner;
+
+		public void Update() {
+			this.enemySpawner.SpawnEnemy();
+		}
+	}
+	
 	public class DebugPlayer : MonoBehaviour {
 		int gold;
 
--- Assets/Scripts/Resources/ResourceUISetup.cs
+++ Assets/Scripts/Resources/ResourceUISetup.cs
@@ -1,17 +1,61 @@
 ﻿using Clicker.ResourceProduction;
+using UnityEditor;
 using UnityEngine;
 
 namespace Resources {
 	public class ResourceUISetup : MonoBehaviour {
 
 		public Resource[] resources;
-		public ResourceUI prefab;
+		public AssetReference resourcePrefab;
 
 		void Start() {
 			foreach (var resource in this.resources) {
-				var instance = Instantiate(this.prefab, this.transform);
+				var instance = Instantiate(this.resourcePrefab.Load<ResourceUI>(), this.transform);
 				instance.SetUp(resource);
 			}
+		}
+	}
+
+	
+	public abstract class AssetReference : ScriptableObject {
+		public abstract T Load<T>() where T : Object;
+	}
+	
+	#if UNITY_EDITOR
+	[CreateAssetMenu]
+	public class AssetReferenceEditor : AssetReference {
+		public UnityEngine.Object reference;
+
+		public override T Load<T>() {
+			return this.reference as T;
+		}
+
+		// Path.Combine prevents these from happening:
+		const string path = "Assets/StreamingAssets//something.png";
+		const string path2 = "Assets/StreamingAssetssomething.png";
+		// If you don't know, which format you get paths in:
+		const string path3 = "Assets/StreamingAssets";
+		const string path4 = "Assets/StreamingAssets/";
+		const string path5 = "Assets\\StreamingAssets\\";
+	}
+	#endif
+	
+	/*
+	 * In between is the difficult part:
+	 * write a script for your build pipeline
+	 * that replaces all AssetReferenceEditor with AssetReferenceBuild
+	 * using this function:
+	 * public static void GetPath(Object reference) {
+			AssetDatabase.GetAssetPath(reference);
+		}
+	 */
+	
+	[CreateAssetMenu]
+	public class AssetReferenceBuild : AssetReference {
+		public string referencePath;
+
+		public override T Load<T>() {
+			return UnityEngine.Resources.Load<T>(this.referencePath);
 		}
 	}
 }
